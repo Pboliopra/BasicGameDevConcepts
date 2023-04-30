7151ba7d2e2d1096f512eedb8ce04f89878b490f
Pboliopra <a01782428@tec.mx>
2023-04-29 22:28:36 -0600

    We be explained and full of sound

--- Basic Game/Assets/Scripts/RupeeObject.cs
+++ Basic Game/Assets/Scripts/RupeeObject.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RupeeObject : MonoBehaviour, IScoringObject {
-    [SerializeField] Rupee rupee;
-    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Rupee rupee; // declares rupee object
+    [SerializeField] SpriteRenderer spriteRenderer; // calls the sprite renderer of the rupee
 
-    private int value;
+    private int value; // holds the points of a rupee
 
+    // Sets the value and sprite to be rendered when the game starts
     void Start() {
         value = rupee.value;
         spriteRenderer.sprite = rupee.sprite;
     }
-
+    // Gets the points of each rupee
     public int GetValue() {
         return value;
     }
